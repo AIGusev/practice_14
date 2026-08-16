1c5c493a26158979f3e0ad437935a3fe07956897
AIGusev <artemgusevjob94@gmail.com>
2026-08-16 22:41:37 +0300

    Update DiscountCalculator.java

--- zadanie2/DiscountCalculator.java
+++ zadanie2/DiscountCalculator.java
@@ -1,21 +1,31 @@
 package practice_14.zadanie2;
 
 public class DiscountCalculator {
+
     private final double LOYAL_FIRST_DISCOUNT = 0.10;
     private final double LOYAL_DISCOUNT = 0.05;
     private final double COUPON_DISCOUNT = 0.07;
     private final double DISCOUNT = 0.02;
 
-    public double calculateDiscount(double price, boolean isLoyalCustomer, boolean isFirstPurchase, boolean hasCoupon) {
+    public double calculateDiscount(double price, boolean isLoyalCustomer,
+                                    boolean isFirstPurchase, boolean hasCoupon) {
 
         if (price <= 0) {
             return 0;
         }
 
+        double discountRate;
 
-        return price;
+        if (isLoyalCustomer && isFirstPurchase) {
+            discountRate = LOYAL_FIRST_DISCOUNT;
+        } else if (isLoyalCustomer) {
+            discountRate = LOYAL_DISCOUNT;
+        } else if (hasCoupon) {
+            discountRate = COUPON_DISCOUNT;
+        } else {
+            discountRate = DISCOUNT;
+        }
+
+        return price - (price * discountRate);
     }
-
-
-
 }
